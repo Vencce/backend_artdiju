8a367912dca86bb4e78a57b750e7be076fa6b061
vencce <vitorferreira20005@gmial.com>
2025-12-29 11:16:01 -0300

    Refactor database configuration in settings.py to use dj_database_url for improved flexibility and connection management

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,10 +69,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Configuração de Banco de Dados
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Validação de Senhas
